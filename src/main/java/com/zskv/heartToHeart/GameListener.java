87d868ea0ea3eb5ae66de6be9b51020d1d1ab0f2
Z <zskv.twitch@gmail.com>
2026-07-10 14:37:56 -0500

    Update 2

--- src/main/java/com/zskv/heartToHeart/GameListener.java
+++ src/main/java/com/zskv/heartToHeart/GameListener.java
@@ -1,7 +1,7 @@
 package com.zskv.heartToHeart;
 
-import org.bukkit.GameMode;
-import org.bukkit.Location;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,11 +10,15 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
 
 public class GameListener implements Listener {
 
     private final PlayerDataManager dataManager;
+    private final Map<UUID, String> pendingTitles = new HashMap<>();
 
     public GameListener(PlayerDataManager dataManager) {
         this.dataManager = dataManager;
@@ -46,12 +50,29 @@ public class GameListener implements Listener {
         Location deathLocation = player.getLocation();
         player.getWorld().strikeLightningEffect(deathLocation);
 
+        Player killer = player.getKiller();
+        if (killer != null) {
+            PlayerData killerData = dataManager.get(killer.getUniqueId());
+            if (killerData.isAlive() && killerData.getHearts() > 1) {
+                killerData.setHearts(killerData.getHearts() - 1);
+                HealthUtil.applyHearts(killer, killerData.getHearts());
+                killer.sendTitle(" ", ChatColor.GREEN + "-1 heart", 0, 30, 10);
+            }
+        }
+
         // hearts doubles as the current life number, so dying on life 10 eliminates
         if (data.getHearts() >= PlayerDataManager.ELIMINATION_LIFE) {
             data.setAlive(false);
             player.setGameMode(GameMode.SPECTATOR);
+            pendingTitles.put(player.getUniqueId(), ChatColor.RED + "ELIMINATED");
+
+            for (Player online : Bukkit.getOnlinePlayers()) {
+                online.playSound(online.getLocation(), Sound.ENTITY_WARDEN_SONIC_BOOM, 1.0f, 1.0f);
+            }
+            new EliminationBroadcastTask().start(dataManager.getPlugin());
         } else {
             data.setHearts(data.getHearts() + 1);
+            pendingTitles.put(player.getUniqueId(), ChatColor.RED + "+1 heart");
         }
 
         dataManager.save();
@@ -64,11 +85,14 @@ public class GameListener implements Listener {
 
         if (!data.isAlive()) {
             player.setGameMode(GameMode.SPECTATOR);
-            return;
+        } else {
+            HealthUtil.applyHearts(player, data.getHearts());
+            player.setHealth(Objects.requireNonNull(player.getAttribute(Attribute.MAX_HEALTH)).getValue());
         }
-
-        HealthUtil.applyHearts(player, data.getHearts());
-        player.setHealth(Objects.requireNonNull(player.getAttribute(Attribute.MAX_HEALTH)).getValue());
+        String subtitle = pendingTitles.remove(player.getUniqueId());
+        if (subtitle != null) {
+            player.sendTitle(" ", subtitle, 0, 30, 10);
+        }
     }
 }
 
